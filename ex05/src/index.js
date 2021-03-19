0b1dd11b1d1fbc3741bff91805b8ccea0575c3cd
Nuruddin Prljaca <nuni.prljaca@gmail.com>
2021-03-19 09:55:46 +0100

    Added index.js file to src folder

---
+++ ex05/src/index.js
@@ -0,0 +1,10 @@
+var myNestedArray = [
+  ["Toblerone", 5],
+  ["Milka", 3],
+];
+function myNestedFunction(arr) {
+  return myNestedArray;
+}
+
+console.log(myNestedFunction());
+module.exports = myNestedFunction;
